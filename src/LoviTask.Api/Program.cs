022a6c33d79f47e10d4890386011036e17a72dc7
Isabelle Bicudo <isabellebicudo70@gmail.com>
2026-07-03 17:40:18 -0400

    Add DbContext and update UserActivityEvent model; configure EF Core with SQLite

--- src/LoviTask.Api/Program.cs
+++ src/LoviTask.Api/Program.cs
@@ -1,7 +1,9 @@
 using LoviTask.Application.Interfaces;
 using LoviTask.Application.Services;
+using LoviTask.Infrastructure.Data;
 using LoviTask.Infrastructure.Repositories;
 using LoviTask.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,11 +11,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<IBehaviorRepository, InMemoryBehaviorRepository>();
+builder.Services.AddDbContext<LoviTaskDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("LoviTaskDatabase")));
+
+builder.Services.AddScoped<IBehaviorRepository, EfBehaviorRepository>();
 builder.Services.AddSingleton<IBrainDumpAiProvider, BrainDumpAiProvider>();
-builder.Services.AddSingleton<IPersonalizationEngine, PersonalizationEngine>();
+builder.Services.AddScoped<IPersonalizationEngine, PersonalizationEngine>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<LoviTaskDbContext>();
+    dbContext.Database.EnsureCreated();
+}
 
 if (app.Environment.IsDevelopment())
 {
